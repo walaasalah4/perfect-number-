893c4b401367d8dd92a4d3624f8588c6300bc67b
walaasalah4 <121608451+walaasalah4@users.noreply.github.com>
2022-12-29 13:35:46 +0200

    Add files via upload

---
+++ PRO 2 perfect number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PRO_2_perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+
+
+
+            Console.Write(" Enter the Minimum number : ");
+            int N1 = int.Parse(Console.ReadLine());
+            Console.Write(" Enter the Maximum number : ");
+            int N2 = int.Parse(Console.ReadLine());
+            for (int x = N1; x <= N2; x++)
+            {
+                int sum = 0;
+                for (int y = 1; y <= x / 2; y++)
+                {
+
+                    if (x % y == 0)
+                    {
+
+                        sum += y;
+
+                    }
+
+                }
+                if (sum == x)
+                {
+
+                    Console.WriteLine(x + " is prefect ");
+
+                }
+            }
+        }
+    }
+}
